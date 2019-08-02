b2534660ed1a59e77297298199c74fb4ff9aa968
Jake Johnson <kognizant.kog@gmail.com>
2019-08-01 23:48:17 -0400

    freeport.rb update

--- freeport.rb
+++ freeport.rb
@@ -2,16 +2,11 @@ class Freeport < Formula
   desc "freeport will free ports by killing processes listening to them."
   homepage "https://github.com/ofalltrades/freeport"
   url "https://github.com/ofalltrades/homebrew-freeport/blob/master/freeport-0.1.tgz"
-  sha256 "4e8049852ead1c3908b33605b35807c6cd648c639752141355149d8b2995ea04"
-  version "0.1"
+  sha256 "f3d0f3e9f9bb1e635cabeaa552df57978de32cb96374f4a9df40a431a43ef24e"
 
   depends_on "candid82/brew/joker"
 
   def install
     bin.install "freeport"
-  end
-
-  test do
-    assert_equal %x('#{bin}/freeport -v'), "version: 0.1"
   end
 end
