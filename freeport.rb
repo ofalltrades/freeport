616bae3d12f2345c354de4de6b94c772c6ab9ce3
Jake Johnson <kognizant.kog@gmail.com>
2019-08-01 23:09:34 -0400

    update sha again

--- freeport.rb
+++ freeport.rb
@@ -2,7 +2,7 @@ class Freeport < Formula
   desc "freeport will free ports by killing processes listening to them."
   homepage "https://github.com/ofalltrades/freeport"
   url "https://github.com/ofalltrades/freeport/blob/master/freeport-0.1.tar.gz"
-  sha256 "a21e450ccea11620f9dd9c21b966f0156236393c359799d66c9c354970beff1c"
+  sha256 "d3d2342c5e451e5fd612aa4d33e02d88bc488c688f46c98c53b193d63da25c93"
   version "0.1"
 
   depends_on "candid82/brew/joker"
